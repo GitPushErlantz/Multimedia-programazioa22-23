f7f36cbfb9bf7258e0907f7996634ea0463804bb
Erlantz <garate.erlantz@uni.eus>
2022-10-18 07:41:36 +0200

    CommunicateActivities v2

---
+++ CommunicateActivities/app/src/main/java/com/example/communicateactivities/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.communicateactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Deklaratu kodean erabiliko diren elementuak
+        Button btnSend = findViewById(R.id.btnSend); // Elementua deklaratzean R.id.elementuarenIzena erabili
+        EditText edtName = findViewById(R.id.edtName);
+        EditText edtSurname = findViewById(R.id.edtSurname);
+        EditText edtN1 = findViewById(R.id.edtN1);
+        EditText edtN2 = findViewById(R.id.edtN2);
+
+        //OnClickListener bat sortu
+        //Lambda ere erabili daiteke
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edtName.getText().toString(); // getText-ek Editable bat bueltatzen du, gogoratu Stringera pasatzea
+                String surname = edtSurname.getText().toString();
+                String nbat = edtN1.getText().toString();
+                String nbi = edtN2.getText().toString();
+                //Intent bat deklaratu
+                Intent intentSecondActivity = new Intent(MainActivity.this, receiveActivity.class);
+                intentSecondActivity.putExtra("keyName", name); // Sortu balio batzuk intentaren barruan, gero deitu egin beharko ditugu
+                intentSecondActivity.putExtra("keySurname", surname); // Aurrena izena eta gero balioa idazten da, gure kasuan String-ak
+                intentSecondActivity.putExtra("keyNbat", nbat);
+                intentSecondActivity.putExtra("keyNbi", nbi);
+                //Intent-a abiarazi
+                startActivity(intentSecondActivity);
+            }
+        });
+    }
+}
---
+++ CommunicateActivities/app/src/main/java/com/example/communicateactivities/receiveActivity.java
@@ -0,0 +1,32 @@
+package com.example.communicateactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class receiveActivity extends AppCompatActivity {
+
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive);
+        // Deklaratu kodean erabiliko diren elementuak
+        TextView txtName = findViewById(R.id.txtName);
+        TextView txtSurname = findViewById(R.id.txtSurname);
+        TextView txtNbatuketa = findViewById(R.id.txtN1);
+        // Jaso aurreko activitytik bidali den intenteko balioak
+        String name = getIntent().getStringExtra("keyName"); // KONTUZ, hauek ondo idatzi behar dira, IDEak ez ditu errore gisa hartzen
+        String surname = getIntent().getStringExtra("keySurname");
+        String nbat = getIntent().getStringExtra("keyNbat");
+        String nbi = getIntent().getStringExtra("keyNbi");
+        // Jarri balio berri hauek testu gisa
+        txtName.setText(name);
+        txtSurname.setText(surname);
+        int nBatuketa = Integer.parseInt(nbat) + Integer.parseInt(nbi); //zenbakien batuketa egiteko
+        txtNbatuketa.setText(Integer.toString(nBatuketa));
+    }
+}
